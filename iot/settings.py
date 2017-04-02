1ba033e722e4b52c404fb6b78561bd3cd08a853d
eracle <ercole.adeluca@gmail.com>
2017-04-03 01:52:58 +0200

    collect static

--- iot/settings.py
+++ iot/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
